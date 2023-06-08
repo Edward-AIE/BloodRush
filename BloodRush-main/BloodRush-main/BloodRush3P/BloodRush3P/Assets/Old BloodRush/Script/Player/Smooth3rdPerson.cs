aa3c05e82b25628d65e733a31703009bf2cef6c9
Edward-AIE <s215765@students.aie.edu.au>
2023-06-08 18:54:10 +1000

    fixed camera

--- BloodRush-main/BloodRush-main/BloodRush3P/BloodRush3P/Assets/Old BloodRush/Script/Player/Smooth3rdPerson.cs
+++ BloodRush-main/BloodRush-main/BloodRush3P/BloodRush3P/Assets/Old BloodRush/Script/Player/Smooth3rdPerson.cs
@@ -12,6 +12,8 @@ public class Smooth3rdPerson : MonoBehaviour
     public float CameraRotateSmoothing;
     Vector3 cameraMoveSpeed;
     Vector3 cameraRotateSpeed;
+
+
     void ResetOffsets()
     {
         if (target == null)
@@ -29,10 +31,24 @@ public class Smooth3rdPerson : MonoBehaviour
     {
         if (target == null)
             return;
-        Vector3 desiredPoition = target.position - target.rotation * offset;
+        Vector3 desiredPosition = target.position - target.rotation * offset;
         Quaternion desiredRotation = target.rotation * Quaternion.Euler(angleOffset);
 
-        transform.position = Vector3.SmoothDamp(transform.position, desiredPoition, ref cameraMoveSpeed, CameraMoveSmoothing);
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref cameraMoveSpeed, CameraMoveSmoothing);
+        transform.rotation = SmoothlyRotate(transform.rotation, desiredRotation, ref cameraRotateSpeed, CameraRotateSmoothing);
+    }
+
+    Quaternion SmoothlyRotate(Quaternion from, Quaternion to, ref Vector3 velocity, float smoothTime)
+    {
+        Vector3 desiredAngles = to.eulerAngles;
+        Vector3 currentAngles = from.eulerAngles;
+        Vector3 newAngles = new Vector3();
+
+        newAngles.x = Mathf.SmoothDampAngle(currentAngles.x, desiredAngles.x, ref velocity.x, smoothTime);
+        newAngles.y = Mathf.SmoothDampAngle(currentAngles.y, desiredAngles.y, ref velocity.y, smoothTime);
+        newAngles.z = Mathf.SmoothDampAngle(currentAngles.z, desiredAngles.z, ref velocity.z, smoothTime);
+
+        return Quaternion.Euler(newAngles);
     }
 }
 
