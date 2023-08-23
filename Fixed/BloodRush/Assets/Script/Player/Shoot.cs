8b13318b1f38518894068603b2a88ede192444ab
Edward-AIE <s215765@students.aie.edu.au>
2023-08-23 13:06:48 +1000

    oyfu

--- Fixed/BloodRush/Assets/Script/Player/Shoot.cs
+++ Fixed/BloodRush/Assets/Script/Player/Shoot.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    [SerializeField] private GameObject bullet;
+    [SerializeField] private float shotForce;
+    [SerializeField] private Rigidbody bullet;
+    private Transform instLocation;
+    private Vector3 shotDirection;
+    private Transform cam;
+    private RaycastHit hit;
+
+    private void Start()
+    {
+        cam = Camera.main.GetComponent<Transform>();
+        instLocation = GameObject.Find("Instantiate Location").GetComponent<Transform>();
+    }
 
     void Update()
     {
@@ -13,11 +24,18 @@ public class Shoot : MonoBehaviour
             ShootGun();
             StartCoroutine(ShootCooldown());
         }
+
+        Physics.Raycast(cam.position, cam.transform.forward, out hit, Mathf.Infinity);
+
+        Vector3 hitSpot = hit.transform.position;
+
+        shotDirection = new Vector3(cam.position.x - hitSpot.x, cam.position.y - hitSpot.y, cam.position.z - hitSpot.z).normalized;
     }
 
     void ShootGun()
     {
-
+        Rigidbody bulletRb = Instantiate(bullet, instLocation);
+        bulletRb.AddForce(shotDirection * shotForce, ForceMode.Impulse);
     }
 
     IEnumerator ShootCooldown()
